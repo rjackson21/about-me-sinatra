6599cf9269107af3337ecb0deb03ea64d3dc102c
Regina Jackson <rjackson156@gmail.com>
2021-11-28 16:43:38 -0500

    Rename search route

--- app.rb
+++ app.rb
@@ -22,13 +22,6 @@ get '/' do
   erb :home
 end
 
-get '/:song_title' do
-  "Searching for #{params['song_title']}!"
-  Genius.access_token = 'uiIUcF6fqotNWzjONln1077y7jPrnDq46kzwd4ChUZ5_PexdAgSsVGFzoMCOZYi8'
-  songs = Genius::Song.search(params['song_title'])
-  erb :song_title, locals: {songs: songs}
-end
-
 get '/home' do
   erb :home
 end
@@ -39,6 +32,13 @@ end
 
 get '/contact' do
   erb :contact
+end
+
+get '/search/:song_title' do
+  "Searching for #{params['song_title']}!"
+  Genius.access_token = 'uiIUcF6fqotNWzjONln1077y7jPrnDq46kzwd4ChUZ5_PexdAgSsVGFzoMCOZYi8'
+  songs = Genius::Song.search(params['song_title'])
+  erb :song_title, locals: {songs: songs}
 end
 
 # This page gets rendered when the user enters an unidentified route
